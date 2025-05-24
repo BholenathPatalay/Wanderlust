800e4f0a0a2ca9fe238551f7c4da90387729f90a
Bholenath Patalay <bholupatalay.1718@gmail.com>
2025-05-24 13:38:25 +0530

    app.js

--- app.js
+++ app.js
@@ -96,8 +96,12 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 app.all(/.*/, (req, res, next) => {
-  next(new ExpressError(404,"Page Not Found"));
+  next(new ExpressError(404, "Page Not Found"));
 });
 
 app.use((err, req, res, next) => {
