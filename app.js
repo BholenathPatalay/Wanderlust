baee1847c9c5a307caf1df9d697671862d039e95
Bholenath Patalay <127043100+BholenathPatalay@users.noreply.github.com>
2025-10-03 10:50:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,111 +8,92 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
 
-const Listing = require("./models/listing.js");
-const Category = require("./models/Category.js");
+const User = require("./models/user");
 
-// Routers
-const listingRouter = require("./routes/listing.js");
-const reviewRouter = require("./routes/review.js");
-const userRouter = require("./routes/user.js");
-
-// DB connection
-const dbUrl = process.env.ATLASDB_URL;
-
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log(" MongoDB connected"))
-.catch(err => console.error(" MongoDB connection error:", err));
-
-// Optional: Auto-seed if RESET_DB=true
-if (process.env.RESET_DB === "true") {
-  console.log(" RESET_DB=true, seeding database...");
-  require("./seeds");
+// Basic error class
+class ExpressError extends Error {
+  constructor(statusCode = 500, message = "Something went wrong") {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
 }
 
+// Connect to MongoDB
+const MONGO_URL = process.env.MONGO_URI || process.env.MONGO_URL || "mongodb://localhost:27017/wanderlust_dev";
+
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Mongo connection error:", err);
+});
+
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use("/public", express.static(path.join(__dirname, "/public")));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-// Session setup
-const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto: { secret: process.env.SECRET },
-  touchAfter: 24 * 3600,
-});
-
-store.on("error", (err) => {
-  console.log("ERROR in MONGO SESSION STORE", err);
-});
-
-const sessionOptions = {
-  store,
-  secret: process.env.SECRET,
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET || "thisshouldbeabettersecret",
   resave: false,
   saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: MONGO_URL }),
   cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-  },
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
 };
 
-app.use(session(sessionOptions));
+app.use(session(sessionConfig));
 app.use(flash());
 
-// Passport config
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Flash & locals
+// flash middleware
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
   next();
 });
 
-// Routes
-app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter);
-app.use("/", userRouter);
-
+// basic home route
 app.get("/", (req, res) => {
-  res.redirect("/listings");
+  res.send("Wanderlust API running");
 });
 
-app.get("/map", (req, res) => {
-  res.render("map");
-});
-
-app.all(/.*/, (req, res, next) => {
+// 404 and error handlers
+app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).render("error.ejs", { err });
+  const { statusCode = 500 } = err;
+  if (res.headersSent) return next(err);
+  res.status(statusCode).send(err.message || "Something went wrong");
 });
 
-// Listen
+// Start server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
+
+module.exports = app;
