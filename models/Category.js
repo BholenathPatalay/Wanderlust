84d71678c981d89afbda53682ec76c30be245562
Bholenath Patalay <127043100+BholenathPatalay@users.noreply.github.com>
2025-10-03 10:44:04 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const categorySchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      unique: true
-    }
-  });
+const categorySchema = new Schema({
+  name: { type: String, required: true, unique: true, trim: true }
+});
 
 module.exports = mongoose.model('Category', categorySchema);
