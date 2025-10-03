0f1701112fdf56d7d6025f2cceb65f61c77773a8
Bholenath Patalay <127043100+BholenathPatalay@users.noreply.github.com>
2025-10-03 10:25:40 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("./models/listing.js");
-const Category = require("./models/Category.js");
-
+const Listing = require("../models/listing.js");
+const Category = require("../models/Category.js");
 const initCategoryData = require('./categories.js');
 
 
