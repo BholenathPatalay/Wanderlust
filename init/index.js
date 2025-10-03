78051e073ff07280e17d809c3b807580d9356b05
Bholenath Patalay <127043100+BholenathPatalay@users.noreply.github.com>
2025-10-03 10:13:18 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -5,7 +5,8 @@ const Category = require("../models/Category.js");
 const initCategoryData = require('./categories.js');
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URI;
+
 
 main()
   .then(() => {
