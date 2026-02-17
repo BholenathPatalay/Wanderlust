d034821f8a852d74c043c2fbc69125ac84f5fb13
Bholenath Patalay <127043100+BholenathPatalay@users.noreply.github.com>
2026-02-17 17:55:55 +0530

    Pass search query to listings templates
    Refactor listing controller methods to pass search query to templates.

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,37 +1,21 @@
-const Listing = require("../models/listing");
-const Category = require("../models/Category");
-const { categories } = require("../init/categories");
-
-
 // ================= INDEX =================
-
 module.exports.index = async (req, res) => {
-
     const page = parseInt(req.query.page) || 1;
     const limit = 8;
-
     const selectedCategory = req.query.category || "";
     const search = req.query.search || "";
     const minPrice = req.query.minPrice || "";
     const maxPrice = req.query.maxPrice || "";
 
-
     let filter = {};
 
     if (selectedCategory) {
-
         const categoryDoc = await Category.findOne({
-
             name: selectedCategory
-
         });
-
         if (categoryDoc) {
-
             filter.category = categoryDoc._id;
-
         }
-
     }
 
     if(search){
@@ -58,24 +42,26 @@ module.exports.index = async (req, res) => {
         .skip((page - 1) * limit)
         .limit(limit);
 
-    res.render("listings/index.ejs", { allListings, totalPages, page, selectedCategory, search, minPrice, maxPrice});
-
+    res.render("listings/index.ejs", { 
+        allListings, 
+        totalPages, 
+        page, 
+        selectedCategory, 
+        search, 
+        minPrice, 
+        maxPrice
+    });
 };
-
-
 
 // ================= NEW FORM =================
-
 module.exports.renderNewForm = (req, res) => {
-    res.render("listings/new.ejs");
+    res.render("listings/new.ejs", { 
+        search: req.query.search || ''  // Pass search to template
+    });
 };
 
-
-
 // ================= SHOW =================
-
 module.exports.showListing = async (req, res) => {
-
     let { id } = req.params;
     const listing = await Listing.findById(id)
         .populate("category")
@@ -85,9 +71,7 @@ module.exports.showListing = async (req, res) => {
                 path: "author",
             },
         })
-
         .populate("owner");
-
 
     if (!listing) {
         req.flash("error", "Listing you requested does not exist");
@@ -96,110 +80,68 @@ module.exports.showListing = async (req, res) => {
 
     res.render("listings/show.ejs", {
         listing,
-        mapToken: process.env.MAP_TOKEN
-
+        mapToken: process.env.MAP_TOKEN,
+        search: req.query.search || ''  // Pass search to template
     });
-
 };
 
-
-
 // ================= CREATE =================
-
 module.exports.createListing = async (req, res) => {
-
     let url = req.file.path;
     let filename = req.file.filename;
 
     const newListing = new Listing(req.body.listing);
-
     newListing.owner = req.user._id;
-
     newListing.image = { url, filename };
-
     await newListing.save();
 
     req.flash("success", "New Listing Created!");
-
     res.redirect("/listings");
-
 };
 
-
-
-
 // ================= EDIT FORM =================
-
 module.exports.renderEditForm = async (req, res) => {
-
     let { id } = req.params;
 
     const listing = await Listing.findById(id)
         .populate("category");
 
     if (!listing) {
-
         req.flash("error", "Listing does not exist");
-
         return res.redirect("/listings");
-
     }
 
-    res.render("listings/edit.ejs", { listing });
-
+    res.render("listings/edit.ejs", { 
+        listing,
+        search: req.query.search || ''  // Pass search to template
+    });
 };
 
-
-
 // ================= UPDATE =================
-
 module.exports.updateListing = async (req, res) => {
-
     let { id } = req.params;
 
     let listing = await Listing.findByIdAndUpdate(
-
         id,
-
         { ...req.body.listing },
-
         { new: true }
-
     );
 
-
-
     if (typeof req.file !== "undefined") {
-
         let url = req.file.path;
-
         let filename = req.file.filename;
-
         listing.image = { url, filename };
-
         await listing.save();
-
     }
 
-
     req.flash("success", "Listing Updated!");
-
     res.redirect(`/listings/${id}`);
-
 };
 
-
-
 // ================= DELETE =================
-
 module.exports.destroyListing = async (req, res) => {
-
     let { id } = req.params;
-
     await Listing.findByIdAndDelete(id);
-
     req.flash("success", "Listing Deleted!");
-
     res.redirect("/listings");
-
 };
